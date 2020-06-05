d8273f2d3a81d96758b13a6fc937f58079d8e11e
Brian Moriguchi <brian@beckos.com>
2020-06-04 18:12:10 -0700

    Initial Commit

---
+++ SBM1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SBM1
+//
+//  Created by Tatsuya Moriguchi on 6/4/20.
+//  Copyright © 2020 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
